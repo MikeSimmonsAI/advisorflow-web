e21625a3b2f4017403d3db7cc3f4c25cfbe8281f
Mike Simmons <simmonsmj242@gmail.com>
2026-09-01 14:47:19 -0500

    remove scratch probe script _wd.ps1 (swept into a wip commit; not referenced anywhere)

--- scripts/_wd.ps1
+++
@@ -1,7 +0,0 @@
-Start-Sleep -Seconds 570
-Set-Location "C:\Users\simmo\OneDrive\Desktop\Web AvdvisorFlow Package\files\advisorflow-web"
-Get-Content ".deploy.log" -Tail 8
-Write-Output "--- log ---"
-git log --oneline -1 | ForEach-Object { $_.Substring(0, [Math]::Min(80, $_.Length)) }
-Write-Output "--- status ---"
-git status --porcelain
